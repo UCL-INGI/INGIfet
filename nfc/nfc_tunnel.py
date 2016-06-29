2d8b7bf122aabb2f4ed0c4d1f7995c236e6e45b1
Mathieu Xhonneux <m.xhonneux@gmail.com>
2016-06-29 10:51:14 +0200

    update nfc

--- nfc/nfc_tunnel.py
+++ nfc/nfc_tunnel.py
@@ -1,4 +1,4 @@
-import nfc, urllib2, binascii, traceback, datetime
+import nfc, urllib2, binascii, traceback, datetime, time
 
 
 BASE_URL = 'http://localhost:8080/{id}'
@@ -9,8 +9,11 @@ def nfc_tag_connected(tag):
     try:
         response = urllib2.urlopen(BASE_URL.format(id=id))
         print("[{}] Request sent for {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), id))
+        time.sleep(5)
     except Exception as e:
         traceback.print_exc()
 
+
+printf("[{}] Starting NFC tunnel ...".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 while 1:
     clf.connect(rdwr={'on-connect': nfc_tag_connected})
